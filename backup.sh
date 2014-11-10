2cba75d72a5b2866bf0b4f8337cb754b857ee0fa
larsfrommars <larsfrommars@gmail.com>
2014-11-10 12:07:24 +0100

    init

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+cd
+cd temp
+rm -rf github-backup-data
+mkdir github-backup-data
+cd github-backup-data
+
+git clone https://github.com/elelpublic/github-backup.git
+git clone https://github.com/elelpublic/wikipower.git
+git clone https://github.com/elelpublic/infomarket.git
+git clone https://github.com/elelpublic/bsmicons.git
+git clone https://github.com/elelpublic/tablestream.git
+git clone https://github.com/elelpublic/samplerestapi.git
+git clone https://github.com/elelpublic/startwithangular.git
+git clone https://github.com/marsfromlars/pixel16pixel.git
+cd ..
+tar czf github-backup-data.tgz github-backup-data
+mv github-backup-data.tgz ~/Dropbox
+
