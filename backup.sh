79055f08f99a5c51a8d33e8518bcec637e39eccc
larsfrommars <larsfrommars@gmail.com>
2016-02-04 13:00:33 +0100

    use google drive and update repo list, also now sort alphabetically

--- backup.sh
+++ backup.sh
@@ -6,18 +6,23 @@ rm -rf github-backup-data
 mkdir github-backup-data
 cd github-backup-data
 
-git clone -q https://github.com/elelpublic/github-backup.git
-git clone -q https://github.com/elelpublic/wikipower.git
-git clone -q https://github.com/elelpublic/infomarket.git
-git clone -q https://github.com/elelpublic/bsmicons.git
-git clone -q https://github.com/elelpublic/tablestream.git
-git clone -q https://github.com/elelpublic/samplerestapi.git
-git clone -q https://github.com/elelpublic/startwithangular.git
+git clone -q https://github.com/elelpublic/beep.git
 git clone -q https://github.com/elelpublic/bsmcommons.git
-git clone -q https://github.com/elelpublic/wikitext-all.git
+git clone -q https://github.com/elelpublic/bsmicons.git
 git clone -q https://github.com/elelpublic/flyer.git
+git clone -q https://github.com/elelpublic/forum.git
+git clone -q https://github.com/elelpublic/github-backup.git
+git clone -q https://github.com/elelpublic/infomarket.git
 git clone -q https://github.com/elelpublic/resthelper.git
+git clone -q https://github.com/elelpublic/samplerestapi.git
+git clone -q https://github.com/elelpublic/spoint.git
+git clone -q https://github.com/elelpublic/startwithangular.git
+git clone -q https://github.com/elelpublic/tablestream.git
+git clone -q https://github.com/elelpublic/wikipower.git
+git clone -q https://github.com/elelpublic/wikitext-all.git
 cd ..
 tar czf github-backup-data.tgz github-backup-data
-mv github-backup-data.tgz ~/Dropbox
+mv github-backup-data.tgz $HOME/Google\ Drive/backup/
+
+rm -rf github-backup-data
 
